387e57cf02b135bd4675b08284e4e758f45d9eda
Ioan311 <81815187+Ioan311@users.noreply.github.com>
2022-05-11 14:26:23 +0300

    Update Audit.java

--- Proiect/src/Audit.java
+++ Proiect/src/Audit.java
@@ -10,8 +10,8 @@ public class Audit {
         Audit aud = new Audit();
         aud.write(actiune, d.format(date));
     }
-    // aici nu am mai stiut sa fac, as fi vrut sa fac ceva de genul: de fiecare data cand avem o actiunde sa vedem de unde face parte si se citeste
-    // fiecare csv file, iar daca actiunea face parte din User... apelez o functie care adauga un nou user in audit.csv
+    // aici nu am mai stiut sa fac, as fi vrut sa fac ceva de genul: de fiecare data cand avem o actiune sa vedem de unde face parte si se citeste
+    // fiecare csv file, iar daca actiunea face parte din User (de exemplu)... apelez o functie care adauga un nou user in audit.csv
     public void write (String actiune, String data) {
         /*Scanner scanner = new Scanner(System.in);
         int caz = scanner.nextInt();
